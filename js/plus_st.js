c4b382b911c20a3ff5240d54a57f6c7bd8091699
EvilMonarch <59070016@it.kmitl.ac.th>
2018-12-18 17:54:08 +0700

    plus
    plus

--- js/plus_st.js
+++ js/plus_st.js
@@ -25,7 +25,7 @@ function test(dt) {
 }
 
 function test3(i) {
-    if (i < 1) return;
+    if (i < 1 ) return;
     setTimeout(function () {
 
         if (document.getElementById("type" + i.toString()).checked) {
@@ -38,7 +38,7 @@ function test3(i) {
             var ttt = 'type' + i.toString();
 
             var std = document.getElementById(ttt).value
-
+            console.log(std)
             var teacher = localStorage.getItem("id")
             db.collection('students').doc(std).collection('subject').doc(namecourse).set({
 
@@ -51,8 +51,8 @@ function test3(i) {
                 room: room
             });
             var subject = firebase.firestore();
-
-
+            
+            
             subject.collection('teacher').doc(localStorage.getItem('id')).collection('subject').doc(localStorage.getItem("namecourse")).get()
                 .then(function (doc) {
                     let dt = []
@@ -63,44 +63,35 @@ function test3(i) {
         }
         test3(--i);
 
-    }, 1000);
+    }, 500);
 
 
 }
 
 var subject = firebase.firestore();
 subject.collection('students')
-    .onSnapshot(function (doc) {
-        var num = 0;
-        doc.docs
-
-            .forEach(data => {
-
-                num++;
-                if (num == 1) {
-                    $('#subject-std').append(createSubject(num, data.data().id, data.data().name)
-                    )
-                } else {
-
-                    $('#subject-std').append(createSubject(num, data.data().id, data.data().name))
-            num = 0;
-        
-    }
-        
-        
-        
+.onSnapshot(function (doc) {
+  var num = 0;
+  doc.docs
+  
+    .forEach(data => {
+      
+        num = num+1;
+        $('#subject-std').append(createSubject(num,data.data().id,data.data().name)
+        )
+      
     })
 })
-function createSubject(num, id, name) {
+function createSubject(num,id,name) {
 
     html = ''
-    html += '<div class="name' + num + '">'
+    html += '<div class="name'+num%2+'">'
+											
+    html += '<input id="type'+num+'" type="checkbox" value="'+id+'"> '+id+' : '+name+'<br>'
 
-    html += '<input id="type' + num + '" type="checkbox" value="' + id + '"> ' + id + ' : ' + name + '<br>'
-
-
+						
     html += '</div>'
-
-
+    
+  
     return html
-}
+  }
